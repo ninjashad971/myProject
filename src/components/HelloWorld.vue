17f1aff60e9958b39a9ec6077092081c25617f5f
ninjashad971 <ninja_shad@hotmail.fr>
2018-02-26 23:41:56 +0100

    init myProject

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <msg-box @changed="updateMsg"></msg-box>
+  </div>
+</template>
+
+<script>
+import MsgBox from './msgBox'
+export default {
+  name: 'HelloWorld',
+  components: { MsgBox },
+  data () {
+    return {
+      msg: 'Welcome to my Vue.js App'
+    }
+  },
+  methods: {
+    updateMsg (value) {
+      if (value) {
+        this.msg = value
+      } else {
+        this.msg = 'Welcome to my Vue.js App'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/msgBox.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="wrap">
+        <input type="text" v-model="msg" placeholder="Entrez un message ici ..." @keyup.enter="changed" />
+        <input type="button" value="OK" @click="changed">
+        <input type="button" value="Reset" @click="reset">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MsgBox',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    changed () {
+      this.$emit('changed', this.msg)
+    },
+    reset () {
+      this.msg = ''
+      this.changed()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrap {
+  text-align: center;
+  margin-top: 3em;
+}
+.wrap input[type="text"] {
+  width: 50%;
+}
+.wrap input[type="button"] {
+  width: 10%;
+  margin-left: 1em;
+}
+</style>
